7cd3e909505e0ef5a4d68c4123cca8ac502789de
Gregory Domingos <58437322+gregdom@users.noreply.github.com>
2022-12-22 08:15:44 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let btnColorGenerator = document.querySelector('section button')
+let h1 = document.querySelector('h1 span')
+let arr = ['a', '0', '1', 'b', '2', '3', 'c', '4', '5', 'd', '6', '7', 'e', '8', '9', 'f']
+
+
+btnColorGenerator.addEventListener('click', () => {
+  let newArr = []
+  let count = 0
+
+  while (count < 6) {
+    let randomNum = parseInt(Math.random() * 16)
+    newArr.push(arr[randomNum])
+    count++
+  }
+
+  let result = `#${newArr.join().replaceAll(',', '')}`
+  document.body.style.backgroundColor = result
+  h1.innerHTML = result
+
+})
